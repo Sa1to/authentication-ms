2df2d696f7cc2641d75b8204cb2c6e39e227e626
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-13 18:38:18 +0100

    Added UserDao class

---
+++ src/main/java/com/szymon/dao/UserDao.java
@@ -0,0 +1,13 @@
+package com.szymon.dao;
+
+import com.szymon.entity.User;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserDao {
+    void saveWithHashedPassword(User user);
+    List<User> findByNameAndSurname(String name, String surname);
+    void delete(User user);
+}
