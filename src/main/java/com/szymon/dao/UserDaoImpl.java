d14baf1c7dc2589a0cf087c2a1b07a3cb9e329c8
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-13 18:39:12 +0100

    Added UserDaoImpl class

---
+++ src/main/java/com/szymon/dao/UserDaoImpl.java
@@ -0,0 +1,39 @@
+package com.szymon.dao;
+
+import com.szymon.entity.User;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private Datastore datastore;
+
+    public void saveWithHashedPassword(User user) {
+        user.setPassword(hashPassword(user.getPassword()));
+        datastore.save(user);
+    }
+
+    public void delete(User user){
+        datastore.delete(user);
+    }
+
+    public List<User> findByNameAndSurname(String name, String surname) {
+        Query<User> query = datastore.find(User.class);
+        query.or(
+                query.criteria("name").equal(name),
+                query.criteria("surname").equal(surname)
+        );
+        return query.asList();
+    }
+
+    private String hashPassword(String passwordToHash) {
+        return BCrypt.hashpw(passwordToHash, BCrypt.gensalt());
+    }
+}
