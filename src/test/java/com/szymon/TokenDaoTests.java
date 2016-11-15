f80dd9db66df31d845c1aa4f919ff11b3f9b1ff2
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-15 21:57:29 +0100

    Added TokenDaoTests class

---
+++ src/test/java/com/szymon/TokenDaoTests.java
@@ -0,0 +1,40 @@
+package com.szymon;
+
+import com.szymon.dao.TokenDao;
+import com.szymon.dao.TokenDaoImpl;
+import com.szymon.entity.Token;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mongodb.morphia.Datastore;
+
+public class TokenDaoTests {
+    @Mock
+    private Datastore datastore;
+
+    @InjectMocks
+    private TokenDaoImpl tokenDao;
+
+    @Mock
+    private Token token;
+
+    @Before
+    public void setUp(){
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void saveTokenTest(){
+        tokenDao.save(token);
+        Mockito.verify(datastore).save(token);
+    }
+
+    @Test
+    public void deleteTokenTest(){
+        tokenDao.delete(token);
+        Mockito.verify(datastore).delete(token);
+    }
+}
