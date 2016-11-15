3af543a78451aaaf039a869ee9bcd096f725a033
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-15 22:48:46 +0100

    Adjusted IntegrationTests

--- src/test/java/com/szymon/IntegrationTests.java
+++ src/test/java/com/szymon/IntegrationTests.java
@@ -15,6 +15,7 @@ import org.junit.runner.RunWith;
 import org.mongodb.morphia.Datastore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -24,7 +25,7 @@ import java.util.ArrayList;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class AuthenticationMsApplicationTests {
+public class IntegrationTests {
 
     @Autowired
     private UserDaoImpl userDao;
@@ -59,9 +60,25 @@ public class AuthenticationMsApplicationTests {
     public void loginAsUserTest() {
         userDao.saveWithHashedPassword(user);
         ResponseEntity response = userController.loginController(user.getLogin(), password);
-        assert(!response.getBody().equals(Responses.WRONG_CREDENTIALS));
+        assert (!response.getBody().equals(Responses.WRONG_CREDENTIALS));
+        assert (response.getStatusCode().equals(HttpStatus.OK));
     }
 
+    @Test
+    public void loginAsUserWithWrongPassword() {
+        userDao.saveWithHashedPassword(user);
+        ResponseEntity responseEntity = userController.loginController(user.getLogin(), "WRONG PASSWORD");
+        assert (responseEntity.getBody().equals(Responses.WRONG_CREDENTIALS));
+        assert (responseEntity.getStatusCode().equals(HttpStatus.BAD_REQUEST));
+    }
+
+    @Test
+    public void loginAsUserWithWrongLogin() {
+        userDao.saveWithHashedPassword(user);
+        ResponseEntity responseEntity = userController.loginController("WRONG LOGIN", user.getPassword());
+        assert (responseEntity.getBody().equals(Responses.WRONG_CREDENTIALS));
+        assert (responseEntity.getStatusCode().equals(HttpStatus.BAD_REQUEST));
+    }
 
 
     @After
