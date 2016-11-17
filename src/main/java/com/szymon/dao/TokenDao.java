a5940b5adf4c0dac8ab58dbab9ece3c2610d86cc
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-17 23:37:09 +0100

    Added new method to TokenDao interface

--- src/main/java/com/szymon/dao/TokenDao.java
+++ src/main/java/com/szymon/dao/TokenDao.java
@@ -1,10 +1,12 @@
 package com.szymon.dao;
 
 import com.szymon.entity.Token;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TokenDao {
     void delete(Token token);
     void save(Token token);
+    Token findByUserId(ObjectId userId);
 }
