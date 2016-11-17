4850baab30ac7d4a9dc34822e2bcc6a5213124f3
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-17 22:51:59 +0100

    added createTokenTest

--- src/test/java/com/szymon/UserAuthServiceTests.java
+++ src/test/java/com/szymon/UserAuthServiceTests.java
@@ -1,9 +1,12 @@
 package com.szymon;
 
+import com.auth0.jwt.JWTSigner;
 import com.szymon.Texts.RoleEnum;
 import com.szymon.dao.TokenDaoImpl;
 import com.szymon.dao.UserDaoImpl;
+import com.szymon.entity.Token;
 import com.szymon.entity.User;
+import com.szymon.jwt.JWTFactory;
 import com.szymon.service.UserAuthService;
 import com.szymon.service.UserAuthServiceImpl;
 import org.apache.commons.lang.RandomStringUtils;
@@ -14,6 +17,9 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import static org.mockito.Matchers.any;
 
 public class UserAuthServiceTests {
 
@@ -23,11 +29,18 @@ public class UserAuthServiceTests {
     @Mock
     private TokenDaoImpl tokenDao;
 
+    @Mock
+    private JWTSigner signer;
+
+    @Mock
+    private JWTFactory jwtFactory;
+
     @InjectMocks
-    private UserAuthService userAuthServiceTests = new UserAuthServiceImpl();
+    private UserAuthService userAuthService = new UserAuthServiceImpl();
 
     private User user;
     private String password = RandomStringUtils.random(7);
+    private String testSecret = "testSecret";
 
     @Before
     public void setup() {
@@ -36,11 +49,23 @@ public class UserAuthServiceTests {
     }
 
     @Test
-    public void authenticateUserTest(){
+    public void authenticateUserTest() {
         User userWithHashedPassword = new User("jankowalski", "Jan", "Kowalski", BCrypt.hashpw(user.getPassword(), BCrypt.gensalt()), RoleEnum.USER);
         Mockito.stub(userDao.findByLogin(user.getLogin())).toReturn(userWithHashedPassword);
-        boolean isAuthenticated = userAuthServiceTests.authenticateUser(user.getLogin(),user.getPassword());
-        assert(isAuthenticated);
+        boolean isAuthenticated = userAuthService.authenticateUser(user.getLogin(), user.getPassword());
+        assert (isAuthenticated);
         Mockito.verify(userDao).findByLogin(user.getLogin());
+    }
+
+    @Test
+    public void createTokenTest() throws Exception {
+        ReflectionTestUtils.setField(userAuthService, "secret", testSecret);
+        String mockToken = "mock token";
+        Mockito.stub(jwtFactory.createJWTSigner(testSecret)).toReturn(signer);
+        Mockito.stub(signer.sign(any())).toReturn(mockToken);
+        Token testToken = new Token(user.getId(), mockToken);
+        Mockito.stub(jwtFactory.createToken(user.getId(),mockToken)).toReturn(testToken);
+        userAuthService.createToken(user);
+        Mockito.verify(tokenDao).save(testToken);
     }
 }
