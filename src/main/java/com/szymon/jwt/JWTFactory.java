250167da86279033dd30c625bf6f5b2262deb415
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-17 22:51:42 +0100

    Added creater token method to JWTFactory

--- src/main/java/com/szymon/jwt/JWTFactory.java
+++ src/main/java/com/szymon/jwt/JWTFactory.java
@@ -1,11 +1,16 @@
 package com.szymon.jwt;
 
 import com.auth0.jwt.JWTSigner;
+import com.szymon.entity.Token;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
 @Component
-public class JWTSignerFactory {
+public class JWTFactory {
     public JWTSigner createJWTSigner(String secret){
         return new JWTSigner(secret);
+    }
+    public Token createToken(ObjectId userId,String jwt){
+        return new Token(userId,jwt);
     }
 }
