b6c5016cf2c11f40fca25d2e8fa81c53fa1e0f3f
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-19 20:13:49 +0100

    Added new response

--- src/main/java/com/szymon/Texts/Responses.java
+++ src/main/java/com/szymon/Texts/Responses.java
@@ -2,4 +2,6 @@ package com.szymon.Texts;
 
 public class Responses {
     public static final String WRONG_CREDENTIALS = "Wrong credentials!";
+    public static final String INACTIVE_USER = "This user is inactive";
+
 }
