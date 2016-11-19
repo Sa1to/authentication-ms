bf03ebdadb7c9bc6789081245bc7210a0088746c
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-19 14:59:16 +0100

    Formated

--- src/main/java/com/szymon/morphia/MorphiaAutoConfiguration.java
+++ src/main/java/com/szymon/morphia/MorphiaAutoConfiguration.java
@@ -18,9 +18,8 @@ public class MorphiaAutoConfiguration {
     private static final String port = "27017";
     private static final String dbname = "auth-ms-db";
 
-    public
     @Bean
-    MongoClient mongoClient() throws UnknownHostException {
+    public MongoClient mongoClient() throws UnknownHostException {
         return (new MongoClient(host + ":" + port));
     }
 
@@ -33,15 +32,13 @@ public class MorphiaAutoConfiguration {
         return datastore;
     }
 
-    public
     @Bean
-    MongoDbFactory mongoDbFactory() throws UnknownHostException {
+    public MongoDbFactory mongoDbFactory() throws UnknownHostException {
         return new SimpleMongoDbFactory(mongoClient(), dbname);
     }
 
-    public
     @Bean
-    MongoTemplate mongoTemplate() throws UnknownHostException {
+    public MongoTemplate mongoTemplate() throws UnknownHostException {
         MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
         return mongoTemplate;
     }
