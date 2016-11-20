a6d2e6fabae6acadc76bee6e369de94934da7a2a
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-20 22:06:46 +0100

    Created new controller method registerUser

--- src/main/java/com/szymon/controller/UserController.java
+++ src/main/java/com/szymon/controller/UserController.java
@@ -1,15 +1,12 @@
 package com.szymon.controller;
 
-import com.szymon.Texts.Responses;
 import com.szymon.Texts.Urls;
-import com.szymon.dao.UserDao;
+import com.szymon.domain.User;
+import com.szymon.service.RegistrationValidator;
 import com.szymon.service.UserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(Urls.AUTH)
@@ -18,9 +15,17 @@ public class UserController {
     @Autowired
     private UserAuthService userAuthService;
 
-    @RequestMapping(Urls.LOGIN)
+    @Autowired
+    private RegistrationValidator registrationValidator;
+
+    @RequestMapping(value = Urls.LOGIN, method = RequestMethod.GET)
     public ResponseEntity loginUser(@RequestParam("login") String login,
                                     @RequestParam("password") String password) {
         return userAuthService.authenticateUser(login, password);
+    }
+
+    @RequestMapping(Urls.REGISTER)
+    public ResponseEntity registerUser(@RequestBody User user){
+        return registrationValidator.validateUserToRegistration(user);
     }
 }
