65aa2b85d9bd0ca293852724ae49a054160cd957
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-20 22:15:46 +0100

    Deleted methods included in AbstractDao

--- src/main/java/com/szymon/dao/TokenDaoImpl.java
+++ src/main/java/com/szymon/dao/TokenDaoImpl.java
@@ -9,24 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TokenDaoImpl implements TokenDao {
+public class TokenDaoImpl extends AbstractDao<Token> implements TokenDao {
     @Autowired
     private Datastore datastore;
 
     private Query<Token> query;
 
     private FieldEnd fieldEnd;
-
-    @Override
-    public void delete(Token token) {
-            datastore.delete(token);
-    }
-
-    @Override
-    public void save(Token token) {
-        datastore.save(token);
-    }
-
+    
     @Override
     public Token findByUserId(ObjectId userId) {
         query = datastore.find(Token.class);
