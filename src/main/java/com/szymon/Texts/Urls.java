963783e8fcdc0a2e7e619cbaed1aca316e3a71ff
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-20 22:08:34 +0100

    Added new uri

--- src/main/java/com/szymon/Texts/Urls.java
+++ src/main/java/com/szymon/Texts/Urls.java
@@ -3,4 +3,5 @@ package com.szymon.Texts;
 public class Urls {
     public static final String AUTH = "/auth";
     public static final String LOGIN = "/login";
+    public static final String REGISTER = "/register";
 }
