1215f21adbae0aebc8b521118a27c8ff5582a65e
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-20 22:08:24 +0100

    Added new response messages

--- src/main/java/com/szymon/Texts/Responses.java
+++ src/main/java/com/szymon/Texts/Responses.java
@@ -3,5 +3,9 @@ package com.szymon.Texts;
 public class Responses {
     public static final String WRONG_CREDENTIALS = "Wrong credentials!";
     public static final String INACTIVE_USER = "This user is inactive";
-
+    public static final String USER_ALREADY_EXIST = "User with that login already exist";
+    public static final String INVALID_LOGIN = "Login is invalid";
+    public static final String INVALID_PASSWORD = "Password is invalid";
+    public static final String ACTIVATION_CODE_SENT = "Activation code has been sent";
+    public static final String ACTIVE_PARAM_NOT_ALLOWED = "Not allowed active parameter";
 }
