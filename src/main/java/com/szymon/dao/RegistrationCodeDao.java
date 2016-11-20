2226c97ec1a5497b1b03c2c3cffbe3d9817a628f
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-21 00:04:33 +0100

    Added RegistrationCodeDao with implementation

---
+++ src/main/java/com/szymon/dao/RegistrationCodeDao.java
@@ -0,0 +1,6 @@
+package com.szymon.dao;
+
+import com.szymon.domain.RegistrationCode;
+
+public interface RegistrationCodeDao extends AbstractDao<RegistrationCode> {
+}
---
+++ src/main/java/com/szymon/dao/RegistrationCodeDaoImpl.java
@@ -0,0 +1,8 @@
+package com.szymon.dao;
+
+import com.szymon.domain.RegistrationCode;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class RegistrationCodeDaoImpl extends AbstractDaoImpl<RegistrationCode> implements RegistrationCodeDao{
+}
