edbf72d363c14649e80bd9b2292e909cb03b01eb
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-21 00:02:26 +0100

    Added new test class RegistrationCodeServiceTests

---
+++ src/test/java/com/szymon/RegistrationCodeServiceTests.java
@@ -0,0 +1,43 @@
+package com.szymon;
+
+import com.szymon.Texts.RoleEnum;
+import com.szymon.dao.RegistrationCodeDao;
+import com.szymon.domain.RegistrationCode;
+import com.szymon.domain.User;
+import com.szymon.service.RegistrationCodeService;
+import com.szymon.service.RegistrationCodeServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import static org.mockito.Matchers.any;
+
+public class RegistrationCodeServiceTests {
+
+    @Mock
+    private RegistrationCodeDao registrationCodeDao;
+
+    @InjectMocks
+    private RegistrationCodeService registrationCodeService = new RegistrationCodeServiceImpl();
+    private User userToRegister;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userToRegister = new User("testlogin123", "testName", "testSurname", "testPassw0rd", RoleEnum.USER, false);
+    }
+
+    @Test
+    public void createAndSaveTest() {
+        registrationCodeService.createAndSave(userToRegister);
+
+        Mockito.verify(registrationCodeDao).save(any());
+    }
+
+//    @Test
+
+
+}
