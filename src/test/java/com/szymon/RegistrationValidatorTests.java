144905e779823c03358789efa8c254cdde24f2ec
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-20 22:08:05 +0100

    Created tests for RegistrationValidatorTests

---
+++ src/test/java/com/szymon/RegistrationValidatorTests.java
@@ -0,0 +1,87 @@
+package com.szymon;
+
+import com.szymon.Texts.Responses;
+import com.szymon.Texts.RoleEnum;
+import com.szymon.dao.UserDao;
+import com.szymon.domain.User;
+import com.szymon.service.RegistrationValidation;
+import com.szymon.service.RegistrationValidator;
+import org.apache.commons.lang.RandomStringUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.Assert.assertEquals;
+
+public class RegistrationValidatorTests {
+
+
+    @Mock
+    private UserDao userDao;
+
+    @InjectMocks
+    private RegistrationValidator registrationValidator = new RegistrationValidation();
+    private User userToRegister;
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        userToRegister = new User("testlogin123", "testName", "testSurname", "testPassw0rd", RoleEnum.USER, false);
+
+    }
+
+    @Test
+    public void validateCorrectUser() {
+        Mockito.stub(userDao.findByLogin(userToRegister.getLogin())).toReturn(null);
+        ResponseEntity responseEntity = registrationValidator.validateUserToRegistration(userToRegister);
+
+        Mockito.verify(userDao).findByLogin(userToRegister.getLogin());
+        assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+    }
+
+    @Test
+    public void validateUserWithIncorrectLogin() {
+        userToRegister.setLogin("shrt");
+
+        ResponseEntity responseEntity = registrationValidator.validateUserToRegistration(userToRegister);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(Responses.INVALID_LOGIN, responseEntity.getBody().toString());
+
+        userToRegister.setLogin(RandomStringUtils.random(21));
+
+        responseEntity = registrationValidator.validateUserToRegistration(userToRegister);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(Responses.INVALID_LOGIN, responseEntity.getBody().toString());
+
+        userToRegister.setLogin("login with space");
+
+        responseEntity = registrationValidator.validateUserToRegistration(userToRegister);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(Responses.INVALID_LOGIN, responseEntity.getBody().toString());
+
+        userToRegister.setLogin("");
+
+        responseEntity = registrationValidator.validateUserToRegistration(userToRegister);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(Responses.INVALID_LOGIN, responseEntity.getBody().toString());
+    }
+
+    @Test
+    public void validateUserWithIncorrectPassword() {
+        userToRegister.setPassword("shortpa");
+
+        ResponseEntity responseEntity = registrationValidator.validateUserToRegistration(userToRegister);
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.getStatusCode());
+        assertEquals(Responses.INVALID_PASSWORD, responseEntity.getBody().toString());
+    }
+}
