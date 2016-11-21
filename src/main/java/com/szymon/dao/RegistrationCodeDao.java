26cdfe4188782807c6773a5eae5b3577f2b45eb2
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-21 22:40:17 +0100

    Added findByUserId method to RegistrationCodeDao and its implementation

--- src/main/java/com/szymon/dao/RegistrationCodeDao.java
+++ src/main/java/com/szymon/dao/RegistrationCodeDao.java
@@ -1,6 +1,9 @@
 package com.szymon.dao;
 
 import com.szymon.domain.RegistrationCode;
+import org.bson.types.ObjectId;
 
 public interface RegistrationCodeDao extends AbstractDao<RegistrationCode> {
+
+    RegistrationCode findByUserId(ObjectId userId);
 }
--- src/main/java/com/szymon/dao/RegistrationCodeDaoImpl.java
+++ src/main/java/com/szymon/dao/RegistrationCodeDaoImpl.java
@@ -1,8 +1,28 @@
 package com.szymon.dao;
 
 import com.szymon.domain.RegistrationCode;
+import com.szymon.domain.User;
+import org.bson.types.ObjectId;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.FieldEnd;
+import org.mongodb.morphia.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class RegistrationCodeDaoImpl extends AbstractDaoImpl<RegistrationCode> implements RegistrationCodeDao{
+    @Autowired
+    private Datastore datastore;
+
+    private Query<RegistrationCode> query;
+
+    private FieldEnd fieldEnd;
+
+    @Override
+    public RegistrationCode findByUserId(ObjectId userId) {
+        query = datastore.find(RegistrationCode.class);
+        fieldEnd = query.field("userId");
+        fieldEnd.equal(userId);
+        return query.get();
+    }
 }
