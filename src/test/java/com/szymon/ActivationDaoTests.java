661aa0ba4c0b8708971860f48bff01c63adf7a2b
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-22 22:16:22 +0100

    Added new test class ActivationDaoTests

---
+++ src/test/java/com/szymon/ActivationDaoTests.java
@@ -0,0 +1,54 @@
+package com.szymon;
+
+import com.szymon.dao.ActivationCodeDao;
+import com.szymon.dao.ActivationCodeDaoImpl;
+import com.szymon.domain.ActivationCode;
+import org.bson.types.ObjectId;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.mongodb.morphia.Datastore;
+import org.mongodb.morphia.query.FieldEnd;
+import org.mongodb.morphia.query.Query;
+
+public class ActivationDaoTests {
+    @Mock
+    private Datastore datastore;
+
+    @Mock
+    private ActivationCode activationCode;
+
+    @Mock
+    private Query<ActivationCode> query;
+
+    @Mock
+    private FieldEnd fieldEnd;
+
+    @InjectMocks
+    private ActivationCodeDao activationCodeDao = new ActivationCodeDaoImpl();
+
+    private ObjectId testUserId = new ObjectId();
+
+    @Before
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void findByUserId() {
+        Mockito.stub(datastore.find(ActivationCode.class)).toReturn(query);
+        Mockito.stub(query.criteria("userId")).toReturn(fieldEnd);
+        Mockito.stub(fieldEnd.equal(testUserId)).toReturn(activationCode);
+
+        activationCodeDao.findByUserId(testUserId);
+
+        Mockito.verify(datastore).find(ActivationCode.class);
+        Mockito.verify(query).criteria("userId");
+        Mockito.verify(fieldEnd).equal(testUserId);
+        Mockito.verify(query).get();
+    }
+
+}
