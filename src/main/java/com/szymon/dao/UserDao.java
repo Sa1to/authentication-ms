89a714e0e85bec258e2455b51f1b9f4b798693b1
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-26 01:04:08 +0100

    Added findById and updateActivation methods to UserDao

--- src/main/java/com/szymon/dao/UserDao.java
+++ src/main/java/com/szymon/dao/UserDao.java
@@ -2,8 +2,11 @@ package com.szymon.dao;
 
 import com.szymon.domain.Token;
 import com.szymon.domain.User;
+import org.bson.types.ObjectId;
 import org.springframework.stereotype.Repository;
 
 public interface UserDao extends AbstractDao<User> {
     User findByLogin(String login);
+    User findById(ObjectId id);
+    void updateActivation(User user, boolean activation);
 }
--- src/main/java/com/szymon/dao/UserDaoImpl.java
+++ src/main/java/com/szymon/dao/UserDaoImpl.java
@@ -1,9 +1,11 @@
 package com.szymon.dao;
 
 import com.szymon.domain.User;
+import org.bson.types.ObjectId;
 import org.mongodb.morphia.Datastore;
 import org.mongodb.morphia.query.FieldEnd;
 import org.mongodb.morphia.query.Query;
+import org.mongodb.morphia.query.UpdateOperations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Repository;
@@ -18,6 +20,8 @@ public class UserDaoImpl extends AbstractDaoImpl<User> implements UserDao {
 
     private FieldEnd fieldEnd;
 
+    private UpdateOperations updateOperations;
+
     public void save(User user) {
         user.setPassword(hashPassword(user.getPassword()));
         datastore.save(user);
@@ -29,6 +33,21 @@ public class UserDaoImpl extends AbstractDaoImpl<User> implements UserDao {
         fieldEnd = query.criteria("login");
         fieldEnd.equal(login);
         return query.get();
+    }
+
+    @Override
+    public User findById(ObjectId id) {
+        query = datastore.find(User.class);
+        fieldEnd = query.criteria("_id");
+        fieldEnd.equal(id);
+        return query.get();
+    }
+
+    @Override
+    public void updateActivation(User user, boolean activation) {
+        updateOperations = datastore.createUpdateOperations(User.class);
+        updateOperations.set("active", activation);
+        datastore.update(user, updateOperations);
     }
 
     private String hashPassword(String passwordToHash) {
