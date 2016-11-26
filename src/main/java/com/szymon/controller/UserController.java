446f2d390b1600f37ddcb503d9d37baa6ce0c7fd
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-26 01:00:20 +0100

    Added activateUser method to controller

--- src/main/java/com/szymon/controller/UserController.java
+++ src/main/java/com/szymon/controller/UserController.java
@@ -2,6 +2,7 @@ package com.szymon.controller;
 
 import com.szymon.Texts.Uri;
 import com.szymon.domain.User;
+import com.szymon.service.ActivationCodeService;
 import com.szymon.service.RegistrationValidator;
 import com.szymon.service.UserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,14 +19,22 @@ public class UserController {
     @Autowired
     private RegistrationValidator registrationValidator;
 
+    @Autowired
+    private ActivationCodeService activationCodeService;
+
     @RequestMapping(value = Uri.LOGIN, method = RequestMethod.GET)
     public ResponseEntity loginUser(@RequestParam("login") String login,
                                     @RequestParam("password") String password) {
         return userAuthService.authenticateUser(login, password);
     }
 
-    @RequestMapping(Uri.REGISTER)
+    @RequestMapping(value = Uri.REGISTER, method = RequestMethod.POST)
     public ResponseEntity registerUser(@RequestBody User user){
         return registrationValidator.validateUserToRegistration(user);
+    }
+
+    @RequestMapping(value = Uri.ACTIVATE, method = RequestMethod.GET)
+    public ResponseEntity activateUser(@RequestParam("activationCode")String activationCode){
+        return activationCodeService.activateUser(activationCode);
     }
 }
