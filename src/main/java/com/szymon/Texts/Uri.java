e3e93a3c2258dcdf7ee1a21d1224a1c5bdb2a2f5
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-11-29 23:11:21 +0100

    Adde new URI

--- src/main/java/com/szymon/Texts/Uri.java
+++ src/main/java/com/szymon/Texts/Uri.java
@@ -5,4 +5,5 @@ public class Uri {
     public static final String LOGIN = "/login";
     public static final String REGISTER = "/register";
     public static final String ACTIVATE = "/activate";
+    public static final String LOGOUT = "/logout";
 }
