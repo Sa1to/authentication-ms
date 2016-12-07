add2ba4343a8165d3642d50b7dc92ba46615dc7f
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-12-07 22:05:47 +0100

    Updated createJwt and getClaimsFromToken methods and moved claims creating to createJwt method

--- src/main/java/com/szymon/jwt/JWTFactory.java
+++ src/main/java/com/szymon/jwt/JWTFactory.java
@@ -1,17 +1,33 @@
 package com.szymon.jwt;
 
 import com.szymon.domain.Token;
+import com.szymon.domain.User;
 import io.jsonwebtoken.*;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Component;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.Map;
 
 @Component
 public class JWTFactory {
-    public String createJwt(Map<String, Object> claims, String secret) {
+    public String createJwt(User user, String secret) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTimeInMillis(System.currentTimeMillis());
+
+        Date issueAt = calendar.getTime();
+
+        calendar.add(Calendar.MINUTE, 5);// expires claim. In this case the token expires in 5 minutes
+
+        Date expiration = calendar.getTime();
+
         return Jwts.builder()
-                .setClaims(claims)
+                .setExpiration(expiration)
+                .setIssuedAt(issueAt)
+                .claim("userId", user.getId())
+                .claim("login", user.getLogin())
+                .claim("role", user.getRole())
                 .signWith(SignatureAlgorithm.HS512, secret)
                 .compact();
     }
@@ -20,9 +36,9 @@ public class JWTFactory {
         return new Token(userId, jwt);
     }
 
-    public Jws<Claims> createClaims(String secret, String token) throws SignatureException {
-return Jwts.parser()
-        .setSigningKey(secret)
-        .parseClaimsJws(token);
+    public Jws<Claims> getClaimsFromToken(String token, String secret) throws SignatureException {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token);
     }
 }
