9e1c7da736878b0cfba27f2804c35723c1f251ec
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-12-07 22:07:14 +0100

    Updated extractUserIdFromToken method

--- src/main/java/com/szymon/jwt/util/UserIdFromClaimsExtractor.java
+++ src/main/java/com/szymon/jwt/util/UserIdFromClaimsExtractor.java
@@ -1,8 +1,11 @@
 package com.szymon.jwt.util;
 
+import com.szymon.jwt.JWTFactory;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.SignatureException;
 import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedHashMap;
@@ -10,7 +13,11 @@ import java.util.LinkedHashMap;
 @Service
 public class UserIdFromClaimsExtractor {
 
-    public ObjectId extractUserIdFromClaims(Jws<Claims> claims){
+    @Autowired
+    private JWTFactory jwtFactory;
+
+    public ObjectId extractUserIdFromToken(String token, String secret) throws NullPointerException, SignatureException {
+        Jws<Claims> claims = jwtFactory.getClaimsFromToken(token, secret);
         LinkedHashMap<String, Object> userId = (LinkedHashMap<String, Object>) claims.getBody().get("userId");
         return new ObjectId((int) userId.get("timestamp"),
                 (int) userId.get("machineIdentifier"),
