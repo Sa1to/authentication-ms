955882ed270966b8566338e5d9a2fff14639ed5f
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-12-11 20:21:47 +0100

    Added new method updateExpiration and refactored validateToken

--- src/main/java/com/szymon/service/TokenService.java
+++ src/main/java/com/szymon/service/TokenService.java
@@ -1,30 +1,34 @@
 package com.szymon.service;
 
-import com.szymon.domain.Token;
-import com.szymon.jwt.JWTFactory;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.jsonwebtoken.*;
 import org.springframework.stereotype.Service;
 
-import java.util.Calendar;
 import java.util.Date;
+
 
 @Service
 public class TokenService {
 
-    @Autowired
-    private JWTFactory jwtFactory;
-
-    public boolean validateTokenExpirationDate(String token, String secret) {
-        Jws<Claims> claims = jwtFactory.getClaimsFromToken(token, secret);
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        Date currentDate = calendar.getTime();
-
-        Date tokenExpirationDate = claims.getBody().getExpiration();
-
-        return currentDate.before(tokenExpirationDate);
+    public void validateToken(String token, String secret) throws MalformedJwtException, SignatureException, ExpiredJwtException {
+        getClaimsFromToken(token, secret);
     }
 
+    public Jws<Claims> getClaimsFromToken(String token, String secret) throws SignatureException, MalformedJwtException, ExpiredJwtException {
+        return Jwts.parser()
+                .setSigningKey(secret)
+                .parseClaimsJws(token);
+    }
+
+    public String updateExpiration(String token, String secret, Date date){
+        Jws<Claims> claims = getClaimsFromToken(token, secret);
+
+        return Jwts.builder()
+                .setExpiration(date)
+                .setIssuedAt(claims.getBody().getIssuedAt())
+                .claim("userId", claims.getBody().get("userId"))
+                .claim("login", claims.getBody().get("login"))
+                .claim("role", claims.getBody().get("role"))
+                .signWith(SignatureAlgorithm.HS512, secret)
+                .compact();
+    }
 }
