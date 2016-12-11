8f62bb3a3a6c07723a3a4a7dd7e572e8d6312cb1
DESKTOP-HOF5F5R\szpaw <szpawlak@gmail.com>
2016-12-11 20:18:32 +0100

    Added new controller method renewToken, set RequestHeaders to controllers that accept tokens, added producing attribute to all controllers

--- src/main/java/com/szymon/controller/UserController.java
+++ src/main/java/com/szymon/controller/UserController.java
@@ -5,6 +5,7 @@ import com.szymon.domain.Credentials;
 import com.szymon.domain.User;
 import com.szymon.service.ActivationCodeService;
 import com.szymon.service.RegistrationValidator;
+import com.szymon.service.TokenRenewService;
 import com.szymon.service.UserAuthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,28 +24,36 @@ public class UserController {
     @Autowired
     private ActivationCodeService activationCodeService;
 
-    @RequestMapping(value = Uri.LOGIN, method = RequestMethod.POST)
+    @Autowired
+    private TokenRenewService tokenRenewService;
+
+    @RequestMapping(value = Uri.LOGIN, method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity loginUser(@RequestBody Credentials credentials) {
         return userAuthService.authenticateUserBaseOnCredentials(credentials.getLogin(), credentials.getPassword());
     }
 
-    @RequestMapping(value = Uri.LOGOUT, method = RequestMethod.GET)
-    public ResponseEntity logoutUser(@RequestParam("token") String token) {
+    @RequestMapping(value = Uri.LOGOUT, method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity logoutUser(@RequestHeader("Authorization") String token) {
         return userAuthService.validateAndRemoveToken(token);
     }
 
-    @RequestMapping(value = Uri.REGISTER, method = RequestMethod.POST)
+    @RequestMapping(value = Uri.REGISTER, method = RequestMethod.POST, produces = "application/json")
     public ResponseEntity registerUser(@RequestBody User user) {
         return registrationValidator.validateUserToRegistration(user);
     }
 
-    @RequestMapping(value = Uri.ACTIVATE, method = RequestMethod.GET)
+    @RequestMapping(value = Uri.ACTIVATE, method = RequestMethod.GET, produces = "application/json")
     public ResponseEntity activateUser(@RequestParam("activationCode") String activationCode) {
         return activationCodeService.activateUser(activationCode);
     }
 
-    @RequestMapping(value = Uri.AUTHENTICATE, method = RequestMethod.GET)
-    public ResponseEntity authenticateUser(@RequestParam("token") String token){
+    @RequestMapping(value = Uri.AUTHENTICATE, method = RequestMethod.GET, produces = "application/json")
+    public ResponseEntity authenticateUser(@RequestHeader("Authorization") String token) {
         return userAuthService.authenticateUserBaseOnToken(token);
+    }
+
+    @RequestMapping(value = Uri.RENEW, method = RequestMethod.GET)
+    public ResponseEntity renewToken(@RequestHeader("Authorization") String token) {
+        return tokenRenewService.renewTokenExpirationDate(token);
     }
 }
